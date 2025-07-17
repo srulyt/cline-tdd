58ea2af9a5d5ac9a0801653cbd77ed784aff7d38
Sruly Taber <srulyt@microsoft.com>
2025-07-17 11:38:49 +0300

    mapper outline

---
+++ src/RefMapper/Mapper.cs
@@ -0,0 +1,25 @@
+﻿namespace Microsoft.EE.ReflectionWorkshop.RefMapper;
+
+public class Mapper
+{
+    public bool CanMap(Type sourceType, Type targetType)
+    {
+       throw new NotImplementedException();
+    }
+
+    public void Map(object source, object target)
+    {
+        throw new NotImplementedException();
+    }
+
+    public object Map(Type sourceType, object source, Type targetType)
+    {
+        throw new NotImplementedException();   
+    }
+
+    public void Map(Type sourceType, object source, Type targetType, object target)
+    {
+        throw new NotImplementedException();
+    }
+
+}
